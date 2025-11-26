1e3b9c54fdd94b6f11a3959e2c5afe0418b5d26c
Samet Topal <astopaal@gmail.com>
2025-11-26 21:53:44 +0300

    feat: add language switcher to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
+import { LanguageSwitcher } from './components/shared/LanguageSwitcher';
 import './App.css';
 
 function App(): JSX.Element {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <LanguageSwitcher />
+    </>
+  );
 }
 
 export default App;
