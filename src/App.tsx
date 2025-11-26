26e8c24df34dfc09eed2bf32ee5e839d2de52e5f
Samet Topal <astopaal@gmail.com>
2025-11-27 00:22:51 +0300

    feat: add toast notifications with sonner for error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 import { LanguageSwitcher } from './components/shared/LanguageSwitcher';
+import { Toaster } from './components/ui/sonner';
 import './App.css';
 
 function App(): JSX.Element {
@@ -8,6 +9,7 @@ function App(): JSX.Element {
     <>
       <RouterProvider router={router} />
       <LanguageSwitcher />
+      <Toaster />
     </>
   );
 }
