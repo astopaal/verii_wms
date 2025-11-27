615879aff476be7821d690ce83cd0ea655e6dcdc
Samet Topal <astopaal@gmail.com>
2025-11-28 00:04:37 +0300

    feat: dark theme entegrasyonu eklendi

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider } from './components/theme-provider';
 import './index.css';
 import './lib/i18n';
 import App from './App.tsx';
@@ -9,7 +10,9 @@ import { queryClient } from './lib/query-client';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <App />
+      </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
 );
