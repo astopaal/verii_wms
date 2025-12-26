8944579da916b250786365f5468a3d2760ca57c1
cannasif-ship-it <can.nasif@v3rii.com>
2025-12-26 07:15:00 -0800

    feat(package): add package module utilities
    - Add query keys constants for TanStack Query

---
+++ src/features/package/utils/query-keys.ts
@@ -0,0 +1,9 @@
+export const PACKAGE_QUERY_KEYS = {
+  HEADERS: 'package-headers',
+  HEADER: 'package-header',
+  PACKAGES: 'package-packages',
+  PACKAGE: 'package-package',
+  LINES: 'package-lines',
+  LINE: 'package-line',
+} as const;
+
